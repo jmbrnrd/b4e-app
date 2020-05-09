ec87c53e4d9cde2c6bebb3d54fbc72cd92d9b5ec
jmbrnrd <jmbarnrd@gmail.com>
2020-05-09 18:45:54 +0100

    added vaults

--- src/app/vaults/vaults.component.ts
+++ src/app/vaults/vaults.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-vaults',
@@ -7,16 +8,40 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class VaultsComponent implements OnInit {
   isLoading = true;
+  clientVaultClientId = 3;
+  privateVaults: Array<any>;
   @Input() loadMessage: string;
 
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
     this.loadMessage = 'One moment whilst we retrieve your private vaults';
     setTimeout(() => {
       this.isLoading = false;
     }, 3000);
+    this.getVaults();
+  }
+
+  getVaults() {
+    return new Promise((resolve, reject) => {
+      this.api.getClientVaults()
+        .toPromise()
+        .then((res: any) => {
+            // Success
+            this.privateVaults = [res[1], res[2]];
+
+            resolve();
+          },
+          err => {
+            // Error
+            reject(err);
+          }
+        );
+    });
+  }
+  loadPrivateVaults() {
+
   }
 
 }

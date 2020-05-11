de270e744303978b40a34362aae8be83d415bf23
jmbrnrd <jmbarnrd@gmail.com>
2020-05-11 16:33:36 +0100

    Render vaults

---
+++ src/app/vaults/vault.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vault',
+  templateUrl: './vault.component.html'
+})
+export class VaultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

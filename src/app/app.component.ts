1c52c02ea2acc12d59e48abff9c9c5b5aefb871d
jmbrnrd <jmbarnrd@gmail.com>
2020-05-11 16:43:15 +0100

    Render vaults

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,10 @@
 import { Component } from '@angular/core';
-import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-
-  constructor( private apiService: ApiService) {
-    this.apiService.getAllClients().subscribe(
-      (res) => {
-        console.log('Clients', res);
-      }
-    );
-    this.apiService.getAllClientVaults().subscribe(
-      (res) => {
-        console.log('Client Vaults', res);
-      }
-    );
-    this.apiService.getAllVaults().subscribe(
-      (res) => {
-        console.log('Vaults', res);
-      }
-    );
+  constructor() {
   }
 }
